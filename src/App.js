7ae5f5998b9aa84e1a792ba69e393538cb136289
Ali AVCI <ali06avci@gmail.com>
2022-05-24 22:45:22 +0300

    added new components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
+import Expenses from "./components/Expenses";
+
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2022, 5, 24),
+    },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.12,
+      date: new Date(2022, 5, 30),
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2022, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk",
+      amount: 450,
+      date: new Date(2022, 5, 12),
+    },
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
     </div>
   );
 }
