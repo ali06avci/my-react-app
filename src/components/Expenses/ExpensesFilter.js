1fe314208db7613c37047b2fb49cc72c901c4dab
Ali AVCI <ali06avci@gmail.com>
2022-06-21 21:33:22 +0300

    updated

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -1,16 +1,16 @@
 import React from "react";
 import "./ExpensesFilter.css";
 
-const ExpensesFilter = () => {
-  const dropDownChangeHandler = (event) => {
-    console.log(event.target.value);
+const ExpensesFilter = (props) => {
+  const dropdownChangeHandler = (event) => {
+    props.onChangeFilter(event.target.value);
   };
 
   return (
     <div className="expenses-filter">
       <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select>
+        <select value={props.selected} onChange={dropdownChangeHandler}>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
